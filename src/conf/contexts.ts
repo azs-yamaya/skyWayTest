89a396d8a35538196f0a3b1b76cf588a5939aed4
Glitch (abrasive-oil-second) <none>
2021-02-18 05:54:18 +0000

    🎅🐸 Checkpoint ./conf.html:28735588/601 ./index.bundle.js:28735588/32829 ./index.html:28735588/561 ./vendor.bundle.js:28735588/436132

--- src/conf/contexts.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import RootStore from "./stores";
-
-const rootStore = new RootStore();
-// @ts-ignore: for debug
-window.store = rootStore;
-
-export const StoreContext = createContext(rootStore);
